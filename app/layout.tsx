40ca39a0e4d6b60415e78cce2806290886f16bca
PutraOFC <115691638+VinnKanaichi@users.noreply.github.com>
2025-07-13 18:34:35 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+// app/layout.tsx
+import './globals.css'
+import { ReactNode } from 'react'
+
+export const metadata = {
+  title: 'Voting Bot',
+  description: 'Voting untuk bot Furina atau Multifungsi',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
